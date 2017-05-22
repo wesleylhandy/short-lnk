10b217e065b325506f8484ccb16807a0c62da246
Wesley Handy <wesleylhandy@gmail.com>
2017-05-21 23:24:07 -0400

    added middleware for redirecting links, added id, shortener/generator

--- server/main.js
+++ server/main.js
@@ -2,22 +2,23 @@ import { Meteor } from 'meteor/meteor';
 import { WebApp } from 'meteor/webapp';
 
 import '../imports/api/users';
-import '../imports/api/links';
+import { Links } from '../imports/api/links';
 import '../imports/startup/simple-schema-configuration.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
-  WebApp.connectHandlers.use((req, res, next)=>{
-    console.log('This is from my custom middleware!');
-    console.log(req.url, req.method, req.headers, req.query);
-    //set HTTP status code
-    // res.statusCode = 404;
-    //set HTTP headers
-    // res.setHeader('my-custom-header', 'Wes was here!');
-    //set HTTP body
-    // res.write('<h1>This is my middleware at work!</h1>');
-    //End HTTP request
-    // res.end();
-    next();
-  })
+ // code to run on server at startup
+  
+  WebApp.connectHandlers.use((req, res, next) => {
+    const _id = req.url.slice(1);
+    const link = Links.findOne({_id});
+
+    if (link) {
+      res.statusCode = 302;
+      res.setHeader('location', link.url);
+      res.end();
+    } else {
+      next();
+    }
+  });
+ 
 });
